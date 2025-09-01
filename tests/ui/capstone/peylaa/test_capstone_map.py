cd943995781beddf7728db54355c6f4bb212aea6
sdobnov.a3 <sdobnov.a3@rwb.ru>
2025-09-01 19:12:47 +0300

    add: 2 new project and fix infra (3)

--- tests/ui/capstone/peylaa/test_capstone_map.py
+++ tests/ui/capstone/peylaa/test_capstone_map.py
@@ -6,12 +6,12 @@ import pytest
 @allure.story("Проверка карты")
 @pytest.mark.smoke
 @pytest.mark.ui
-def test_capstone_map_page():
+def test_capstone_map_page(capstone_map_page):
     """Тест проверки карты проекта Capstone (Peylaa)."""
     # TODO: Реализовать тест после создания фикстур для Capstone
     with allure.step("Открываем карту проекта Capstone"):
-        # capstone_map_page.open_map_page()
-        pass
+        capstone_map_page.open_map_page()
+
 
     with allure.step("Проверяем загрузку карты"):
         # capstone_map_page.check_map_loaded()
--- tests/ui/wellcube/tranquil/test_tranquil_map.py
+++ tests/ui/wellcube/tranquil/test_tranquil_map.py
@@ -8,11 +8,9 @@ import pytest
 @pytest.mark.ui
 def test_tranquil_map_page(wellcube_map_page):
     """Тест карты проекта Wellcube (Tranquil)."""
-    # TODO: Реализовать тест после создания фикстур для Wellcube
     with allure.step("Открываем карту Wellcube (Tranquil)"):
-        # wellcube_map_page.open_map_page()
-        pass
+        wellcube_map_page.open_map_page()
+        wellcube_map_page.check_map_loaded()
 
     with allure.step("Проверяем функциональность карты"):
-        # TODO: Добавить проверки для Wellcube (Tranquil)
         pass
