d13e55cda8a198edb544d31693faa848bcb40331
sdobnov.a3 <sdobnov.a3@rwb.ru>
2025-09-24 14:53:30 +0300

    add: test for tranquil and add marks for PROD

--- tests/ui/wellcube/tranquil/test_tranquil_download_pdf.py
+++ tests/ui/wellcube/tranquil/test_tranquil_download_pdf.py
@@ -1,3 +1,5 @@
+import os
+
 import allure
 import pytest
 
@@ -7,6 +9,10 @@ import pytest
 @pytest.mark.smoke
 @pytest.mark.regression
 @pytest.mark.ui
+@pytest.mark.skipif(
+    os.getenv("TEST_ENVIRONMENT", "dev") == "prod",
+    reason="тест временно отключен на PROD",
+)
 def test_tranquil_download_ownership_offer(wellcube_page, agent_page):
     """Тест скачивания PDF на проектк Tranquil."""
 
